576ee9c015ba7e9d9a16c96b08eac426171f5b4b
cenarius1985 <30703286+cenarius1985@users.noreply.github.com>
2022-10-09 16:22:44 -0300

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCDPhWtRUn_BnosO2rQBYUoqa9F-idXXJU",
+    authDomain: "inventario-8235e.firebaseapp.com",
+    projectId: "inventario-8235e",
+    storageBucket: "inventario-8235e.appspot.com",
+    messagingSenderId: "770244062123",
+    appId: "1:770244062123:web:5add740fecd4bbbb6b00ee",
+    measurementId: "G-QVXKX5SH61"
+  }
+};
+
